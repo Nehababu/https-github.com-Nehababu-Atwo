4eca7d9bcc404fa11d46267061e98dc9962b1f94
ankita <ankitakumari650@gmail.com>
2019-10-08 19:37:51 +0530

    firstbranch

--- WebAnkita/Controllers/Student.cs
+++ WebAnkita/Controllers/Student.cs
@@ -10,5 +10,6 @@ namespace WebAnkita.Controllers
         public string name { get; set; }
         public string wriyt { get; }
         public string branch { get; set; }
+        public int StudentNumber { get; set; }
     }
 }

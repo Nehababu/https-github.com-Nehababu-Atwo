b51ecb7df4bb11a20ed30d5fc7af3a757cb8dd09
ankita <ankitakumari650@gmail.com>
2019-10-08 19:32:31 +0530

    sictg

--- WebAnkita/Controllers/Student.cs
+++ WebAnkita/Controllers/Student.cs
@@ -9,6 +9,6 @@ namespace WebAnkita.Controllers
     {
         public string name { get; set; }
         public string wriyt { get; }
-        public string bdv { get; set; }
+        public string branch { get; set; }
     }
 }
